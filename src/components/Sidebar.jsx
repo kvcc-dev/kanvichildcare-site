234d02d3efda210d76008320e66835dee1ec8b2d
Rushikesh Wasule <rushikesh.wasule@ibm.com>
2026-02-14 00:18:35 +0530

    Initial Commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2 className="logo">My Dashboard</h2>
+      <ul className="nav-list">
+        <li>Home</li>
+        <li>Analytics</li>
+        <li>Reports</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
